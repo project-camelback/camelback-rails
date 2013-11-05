b799cf5cda1ea374988534504651d5911414beba
Theo Vora <theevo@gmail.com>
2013-11-04 22:12:14 -0500

    remove binding.pry

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
 
   def login
-    # binding.pry
     url = "https://github.com/login/oauth/authorize?client_id=#{GITHUB_CAMELBACK_OAUTH_CLIENT_ID}&scope=repo,gist"
     redirect_to url
   end
