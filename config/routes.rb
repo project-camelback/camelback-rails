ea0cc070722d4bbd472cc7181eae44abebc1a4ee
irmiller22 <irmiller22@gmail.com>
2013-11-09 14:11:18 -0500

    changed Signin/Signout routes to Login/Logout, fixed callback route

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,10 @@ WebAppRails::Application.routes.draw do
   resources :assignments
 
   # OMNIAUTH
-  get 'auth/:provider/callback', to: 'sessions#create'  
-  get '/signin' => redirect('/auth/github')
-  get '/signout', to: 'sessions#destroy', as: :signout
+  # get 'auth/:provider/callback', to: 'sessions#create'  
+  get '/callback', to: 'sessions#create'
+  get '/login' => redirect('/auth/github')
+  get '/logout', to: 'sessions#destroy', as: :logout
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
