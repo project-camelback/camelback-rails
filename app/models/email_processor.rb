e8581c6f03fbd7ca3310c5d8546d06b51620e508
github username <github email address>
2013-11-10 17:26:09 -0500

    fixed piazza integration

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -8,7 +8,7 @@ class EmailProcessor < ActiveRecord::Base
       end
       if email.subject.include?("Lab" || "lab" || "HW")
         assignment = Assignment.new
-        assignment.name = email.header
+        assignment.name = email.subject
         assignment.description = email.body
         assignment.save
       end
