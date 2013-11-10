3dcabc991b66c74c02d1297af373c8f241d71c79
irmiller22 <irmiller22@gmail.com>
2013-11-10 15:05:32 -0500

    changed routes and omniauth files to show relative paths for redirects

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_CAMELBACK_OAUTH_CLIENT_ID'], ENV['GITHUB_CAMELBACK_OAUTH_CLIENT_SECRET'], scope: "user,repo,gist", redirect_uri: 'http://camelback-app.herokuapp.com/auth/github/callback'
+  provider :github, ENV['GITHUB_CAMELBACK_OAUTH_CLIENT_ID'], ENV['GITHUB_CAMELBACK_OAUTH_CLIENT_SECRET'], scope: "user,repo,gist", redirect_uri: '/auth/github/callback'
 end	
