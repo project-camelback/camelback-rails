21fa683320749f3b7dcac8265c478bd8579f4a5f
github username <github email address>
2013-11-10 17:30:14 -0500

    fixed piazza integration

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -1,12 +1,12 @@
 class EmailProcessor < ActiveRecord::Base
    def self.process(email)
-       if email.subject.include? "The Plan"
-        plan = Plan.new
-        plan.header = email.subject
-        plan.content = email.body
-        plan.save
-      end
-      if email.subject.include?("Lab" || "lab" || "HW")
+      #  if email.subject.include? "The Plan"
+      #   plan = Plan.new
+      #   plan.header = email.subject
+      #   plan.content = email.body
+      #   plan.save
+      # end
+      # if email.subject.include?("Lab" || "lab" || "HW")
         assignment = Assignment.new
         assignment.name = email.subject
         assignment.description = email.body
