d5fcc3a626ce121dba6b9a00a6db6fbd50fde0a6
irmiller22 <irmiller22@gmail.com>
2013-11-12 18:37:34 -0500

    changes to branching in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,20 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_user
-  helper_method :student_user
+  # helper_method :student_user
 
   private
     def current_user
       if session[:user_id]
         @current_user ||= User.find(session[:user_id])
-      else
-        @current_user ||= User.find(session[:user_id]) if session[:user_id]
       end
-
     end
     
     def student_user
     	current_user
-    	@student_user = Student.find_by(:name => @current_user.login)
+    	@student_user = Student.find_by(:name => @current_user.login) if current_user
     end
 end
