bc9de65bbddce460e7ff36c4fea6192f7de428d5
The one, The only, Mike Spangler <spangler.mike@gmail.com>
2013-11-12 18:49:12 -0500

    add instructor attribute to assignment model.  add logic to populate that attr via email_processor

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -9,16 +9,18 @@ class EmailProcessor < ActiveRecord::Base
          plan.content = string_parse.match(/^(Day(\S|\s)+)^#/)
          plan.save
 
-      elsif subject_parse.match(/(HW|hw|lab|Lab|Hw)/)
+      elsif subject_parse.match(/(HW|hw|Hw|lab|Lab)/)
          assignment = Assignment.new
-         
          if string_parse.match(/(https*:\/\/github.com\/(flatiron-school\/([\w-]+)))/)
             url, full_name, name = string_parse.match(/(https*:\/\/github.com\/(flatiron-school\/([\w-]+)))/).captures
             assignment.name = name #bare repo name
             assignment.url = url #url for flatiron fork
             assignment.full_name = full_name #repo name with org
+            assignment.instructor = string_parse.match(/Avi Flombaum|Scott C. Reynolds|Jonathan Grover|Spencer Rogers/)
             assignment.save
          end
+      # else
+      #    note = Note.new
       end
    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131110194531) do
+ActiveRecord::Schema.define(version: 20131112225456) do
 
   create_table "assignments", force: true do |t|
     t.string   "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20131110194531) do
     t.datetime "due_date"
     t.string   "evaluation_type"
     t.string   "branch"
+    t.string   "instructor"
   end
 
   create_table "email_processors", force: true do |t|
