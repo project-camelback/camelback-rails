e3caf7dee53b7e59f853cc93cf26f97da943240d
The one, The only, Mike Spangler <spangler.mike@gmail.com>
2013-11-13 09:46:56 -0500

    update config files for logging

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -1,13 +1,12 @@
 class EmailProcessor < ActiveRecord::Base
    def self.process(email)
       
-      subject_parse = email.subject
-      string_parse = email.body
+      subject_parse = email.subject.to_s
+      string_parse = email.body.to_s
 
       if subject_parse.match(/(The Plan)/)
          plan = Plan.new
-         Rails.logger.warn(string_parse.match(/^(Day(\S|\s)+)^#/).to_s)
-         plan.content = string_parse.match(/^(Day(\S|\s)+)^#/).to_s
+         plan.content = string_parse.match(/^(Day(\S|\s)+)^#/)
          Rails.logger.warn(plan.content)       
          plan.save
       end
