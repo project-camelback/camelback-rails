702e53571ea8fb844ceb7fd1362cc734f4af91d6
The one, The only, Mike Spangler <spangler.mike@gmail.com>
2013-11-13 11:24:13 -0500

    wacky regex:

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -7,7 +7,7 @@ class EmailProcessor < ActiveRecord::Base
       if subject_parse.match(/(The Plan)/)
          plan = Plan.new 
          # Rails.logger.warn(string_parse)    
-         plan.content = string_parse.match(/^(Day(\S|\s)+)^#/).to_s
+         plan.content = /^(Day(\S|\s)+)^#/.match(string_parse)
          Rails.logger.warn(plan.content)       
          plan.save
       end
