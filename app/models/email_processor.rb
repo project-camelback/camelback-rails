19f26f2c1a285f231be7bf598af376b149920d37
The one, The only, Mike Spangler <spangler.mike@gmail.com>
2013-11-13 09:02:40 -0500

    hardcode potato

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -6,7 +6,7 @@ class EmailProcessor < ActiveRecord::Base
 
       if subject_parse.match(/(The Plan)/)
          plan = Plan.new
-         plan.content = string_parse.match(/^(Day(\S|\s)+)^#/).to_s
+         plan.content = 'potato' #string_parse.match(/^(Day(\S|\s)+)^#/).to_s 
          plan.save
       end
 
