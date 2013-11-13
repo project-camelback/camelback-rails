141aa6ef4842c346702410ef8fb1224f4661c834
Theo Vora <theevo@gmail.com>
2013-11-13 15:54:45 -0500

    rake grade en masse

--- lib/tasks/grade_hw.rb
+++ lib/tasks/grade_hw.rb
@@ -1,14 +1,13 @@
 class GradeHomeworks
   def initialize()
-    # @assignments = Assignment.where(spec_present: true)
-    @assignment = Assignment.find(9)
+    @assignments = Assignment.where(spec_present: true)
   end
 
   def call
-    # @assignments.each do |assignment|
-    #   puts "Grading #{assignment.name} ..."
-    #   Homework.evaluate_all(assignment)
-    # end
-    Homework.evaluate_all(@assignment)
+    @assignments.each do |assignment|
+      puts "Grading #{assignment.name} ..."
+      Homework.evaluate_all(assignment)
+      sleep(90)
+    end
   end
 end
