703264f943369ecab44f60da4da4608a8fdd6a9d
The one, The only, Mike Spangler <spangler.mike@gmail.com>
2013-11-13 20:58:52 -0500

    create feed model and logic

--- app/models/github_feed.rb
+++ app/models/github_feed.rb
@@ -1,6 +1,22 @@
 class GithubFeed < ActiveRecord::Base
 
-  feed = Feedzirra::Feed.fetch_and_parse("https://github.com/organizations/flatiron-school/mikespangler.private.atom?token=00d44043419043cfec014c990cbb228c")
+  def self.define_feed
+    Feedzirra::Feed.fetch_and_parse("https://github.com/organizations/flatiron-school/mikespangler.private.atom?token=00d44043419043cfec014c990cbb228c")
+  end
+
+  def self.get_feed
+    feed = define_feed
+    feed.entries.each do |entry|
+      entry[].title + feed.entries[0].url
+    feed
+  end
+
+end
+
+# add to view: <%= GithubFeed.get_feed[0].title %>
+
+
+
 
   # puts feed
 
@@ -11,20 +27,11 @@ class GithubFeed < ActiveRecord::Base
   # feed.etag           # => "GunxqnEP4NeYhrqq9TyVKTuDnh0"
   # feed.last_modified  # => Sat Jan 31 17:58:16 -0500 2009 # it's a Time object
 
-  entry = feed.entries.first
-  entry.title      # => "Ruby Http Client Library Performance"
-  entry.url        # => "http://www.pauldix.net/2009/01/ruby-http-client-library-performance.html"
-  entry.author     # => "Paul Dix"
-  entry.summary    # => "..."
-  entry.content    # => "..."
-  puts entry.published  # => Thu Jan 29 17:00:19 UTC 2009 # it's a Time object
-  entry.categories # => ["...", "..."]
-
-  updated_feed = Feedzirra::Feed.update(feed)
-  if updated_feed # => nil if there is nothing new
-    puts updated_feed.new_entries # => [] if nothing new otherwise a collection of feedzirra entries
-    puts updated_feed.etag # => same as before if nothing new. although could change with comments added to entries.
-    puts updated_feed.last_modified # => same as before if nothing new. although could change with comments added to entries.
-  end
-  # updating a single feed
-end
+  
+  # entry.title      # => "Ruby Http Client Library Performance"
+  # entry.url        # => "http://www.pauldix.net/2009/01/ruby-http-client-library-performance.html"
+  # entry.author     # => "Paul Dix"
+  # entry.summary    # => "..."
+  # entry.content    # => "..."
+  # puts entry.published  # => Thu Jan 29 17:00:19 UTC 2009 # it's a Time object
+  # entry.categories # => ["...", "..."]
