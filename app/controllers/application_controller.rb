261e3f4412c8befff369ebad97d3cd901dbbb840
The one, The only, Mike Spangler <spangler.mike@gmail.com>
2013-11-13 22:05:35 -0500

    remove raise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,8 +17,6 @@ class ApplicationController < ActionController::Base
         redirect_to login_path
       else
         @current_user ||= User.find(session[:user_id]) if session[:user_id]
-        raise @current_user.inspect
-        # @grav_url = Student.find_by(:name => current_user.login).assignments.first.gravatar_url
       end
     end
     helper_method :current_user
