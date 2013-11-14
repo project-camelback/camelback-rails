042bfa274291ef22e9bd9d6f3e9d65fbea4cd687
Daniel Chang <daniel.chang85@gmail.com>
2013-11-13 20:44:49 -0500

    issues now working o(^_^)-b

--- app/models/homework.rb
+++ app/models/homework.rb
@@ -17,58 +17,47 @@ class Homework < ActiveRecord::Base
   belongs_to :student
   belongs_to :assignment
 
-  def get_most_recent_issue
-    client = Octokit::Client.new :access_token => session[:token]
-    student = Student.find_by(name: current_user.login)
+  def self.get_most_recent_issue(token, user)
+    client = Octokit::Client.new(:access_token => token)
+    student = Student.find_by(name: user.login)
 
     student_repos = client.repos
-    student_homeworks = Homework.where("student_id = ?", student.id)
 
     student_repos.each do |repo|
       if !user_has_forked_repo?(repo, student).empty?
-        homework = Homework.where("name = ? AND student_id = ?", repo.name, student.id)
+        homework = Homework.where("name = ? AND student_id = ?", repo.name, student.id).first
 
         if issues_set_and_not_empty?(repo)
           homework.current_issue = get_repo_title(repo)
         elsif issues_are_empty?(repo)
-          homework.current_issue = "Currently no issues".
-        rescue Octokit::ClientError => e
-          homework.current_issue = message_to_enable_issues
+          homework.current_issue = "Currently no issues."
+        else
+          homework.current_issue = "To use this feature, enable issues in this repo's settings."
         end
+        homework.save 
       end
     end
   end
 
-  def user_has_forked_repo?(repo, student)
+  def self.user_has_forked_repo?(repo, student)
     Homework.where("name = ? AND student_id = ?", repo.name, student.id)
   end
 
-  def issues_set_and_not_empty?(repo) 
+  def self.issues_set_and_not_empty?(repo) 
     repo.rels[:issues].get && !repo.rels[:issues].get.data.empty?
+    rescue Exception
+    false
   end
 
-  def issues_are_empty?(repo)
+  def self.issues_are_empty?(repo)
     repo.rels[:issues].get.data.empty?
+    rescue Exception
+    false
   end
 
-  def get_repo_title(repo)
+  def self.get_repo_title(repo)
     repo.rels[:issues].get.data.first.title
-  end
-
-  def message_to_enable_issues
-    "To use this feature, enable issues in this repo's settings."
+    rescue Exception
+    false
   end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
